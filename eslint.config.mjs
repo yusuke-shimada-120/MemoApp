398daeb4cda2251ff512dbf9e3754b643b4d70c4
shima <227357455+yusuke-shimada-120@users.noreply.github.com>
2025-08-22 18:33:10 +0900

    Install ESlint

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js"
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+  // rules ここから -----------------------
+  {
+    rules: {
+      "react/react-in-jsx-scope": 0,
+      "semi": [2, "never"],
+      "comma-dangle": [2, "never"]
+    }
+  }
+  // rules ここまで -----------------------
+]
